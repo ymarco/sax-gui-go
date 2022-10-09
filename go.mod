a06226d2a806f389da3fd65deaea660020daab60
Yoav Marco <yoavm448@gmail.com>
2022-10-09 12:19:00 +0300

    Use bleep for sound

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.19
 
 require (
 	gioui.org v0.0.0-20221004231135-80196f3c3ed3
-	github.com/hajimehoshi/oto/v2 v2.3.1
+	github.com/bspaans/bleep v0.0.0-20220414232837-486f92844ed7
 )
 
 require (
@@ -12,9 +12,15 @@ require (
 	gioui.org/shader v1.0.6 // indirect
 	github.com/benoitkugler/textlayout v0.1.3 // indirect
 	github.com/gioui/uax v0.2.1-0.20220819135011-cda973fac06d // indirect
+	github.com/go-audio/audio v0.0.0-20181013203223-7b2a6ca21480 // indirect
+	github.com/go-audio/wav v0.0.0-20181013172942-de841e69b884 // indirect
 	github.com/go-text/typesetting v0.0.0-20220411150340-35994bc27a7b // indirect
+	github.com/mattetti/audio v0.0.0-20180912171649-01576cde1f21 // indirect
+	github.com/veandco/go-sdl2 v0.4.0 // indirect
+	gitlab.com/gomidi/midi v1.13.0 // indirect
 	golang.org/x/exp/shiny v0.0.0-20220827204233-334a2380cb91 // indirect
 	golang.org/x/image v0.0.0-20220722155232-062f8c9fd539 // indirect
 	golang.org/x/sys v0.0.0-20220825204002-c680a09ffe64 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- sound.go
+++ sound.go
@@ -1,125 +1,13 @@
 package main
 
 import (
-	"log"
-	"math"
+	"github.com/bspaans/bleep/audio"
+	"github.com/bspaans/bleep/channels"
+	"github.com/bspaans/bleep/controller"
+	"github.com/bspaans/bleep/generators"
+	"github.com/bspaans/bleep/synth"
 	"time"
-
-	"github.com/hajimehoshi/oto/v2"
 )
-
-// Stolen from Sointu
-//
-// FloatBufferTo16BitLE is a naive helper method to convert []float32 buffers to
-// 16-bit little-endian, but encoded in byte buffer
-//
-// Appends the encoded bytes into "to" slice, allowing you to preallocate the
-// capacity or just use nil
-func FloatBufferTo16BitLE(from []float32, to []byte) []byte {
-	for _, v := range from {
-		var uv int16
-		if v < -1.0 {
-			uv = -math.MaxInt16 // we are a bit lazy: -1.0 is encoded as -32767, as this makes math easier, and -32768 is unused
-		} else if v > 1.0 {
-			uv = math.MaxInt16
-		} else {
-			uv = int16(v * math.MaxInt16)
-		}
-		to = append(to, byte(uv&255), byte(uv>>8))
-	}
-	return to
-}
-
-type SineWave struct {
-	input    *SineWaveInputGenerator
-	smoother Smoother
-	// Volume (up to 1.0)
-	Vol        float32
-	SampleRate int
-}
-
-func NewSineWave(freq, vol float32, sampleRate int) SineWave {
-	input := &SineWaveInputGenerator{a: SineWaveInputCoeff(freq, sampleRate)}
-	return SineWave{Vol: vol, SampleRate: sampleRate,
-		input:    input,
-		smoother: NewSmoother(input, 1)}
-}
-
-func (self *SineWave) Read(buf []byte) (int, error) {
-	bufFloat := make([]float32, cap(buf)/2)
-	// bufFloat [len(buf)/2]float32
-
-	var i int64
-	for i = 0; i < int64(cap(bufFloat)); i++ {
-		bufFloat[i] = float32(math.Sin(
-			2*math.Pi*
-				float64(self.input.apply()))) * self.Vol
-	}
-	FloatBufferTo16BitLE(bufFloat, buf[:0])
-
-	return cap(buf), nil
-}
-
-// An abstruction of a linear function that's meant to be an input to sin().
-type SineWaveInputGenerator struct {
-	// the frequency in Hz
-	a float32
-	// implementation detail to allow smooth transitions
-	b           float32
-	samplesRead int64
-}
-
-func SineWaveInputCoeff(freq float32, sampleRate int) float32 {
-	return freq / float32(sampleRate)
-}
-
-func (self *SineWaveInputGenerator) apply() float32 {
-	res := self.a*float32(self.samplesRead) + self.b
-	self.samplesRead++
-	return res
-}
-
-// If inputted to a sine function, this changes the wave into a new one with
-// frequency aNew, such that the old and new waves connect without jumps.
-func (self *SineWaveInputGenerator) transitionInto(aNew float32) {
-	aOld := self.a
-	bOld := self.b
-	self.a = aNew
-	self.b = aOld*float32(self.samplesRead) + bOld - aNew*float32(self.samplesRead)
-}
-
-type InputGenerator interface {
-	apply() float32
-}
-
-// A smoother takes an src InputGenerator and on each apply() returns src's
-// apply() averaged out with its previous outputs from apply()
-// TODO it's unused currently, there's a bug somewhere in it.
-type Smoother struct {
-	src     InputGenerator
-	history []float32
-	i       int
-}
-
-func NewSmoother(src InputGenerator, historyLen int) Smoother {
-	s := Smoother{src: src, history: make([]float32, historyLen)}
-	// for i := 0; i < historyLen-1; i++ {
-	// 	s.history[i] = src.apply()
-	// }
-	// s.i = historyLen - 1
-	return s
-}
-func (self *Smoother) apply() float32 {
-	self.history[self.i] = self.src.apply()
-	var sum float32 = 0.0
-	for i := 0; i < len(self.history); i++ {
-		sum += self.history[(i+100)%len(self.history)]
-		// log.Println(i)
-		// sum += self.history[i]
-	}
-	self.i = (self.i + 1) % len(self.history)
-	return sum / float32(len(self.history))
-}
 
 // TODO this is a stand-in for midi. If/when we do a proper synthesizer it'd be
 // replaced.
@@ -132,48 +20,41 @@ type Note struct {
 // A note with freq or volume 0 means pause playing.
 // When a value is recieved from quit, return.
 func StreamingPlayer(notes chan Note, quit chan int) {
-	// Prepare an Oto context (this will use your default audio device) that will
-	// play all our sounds. Its configuration can't be changed later.
+	// bleep setup copied from their examples/sinewave/sinewave.go
+	cfg := audio.NewAudioConfig()
+	instr := generators.NewSineWaveOscillator
+	channel := channels.NewPolyphonicChannel()
+	channel.SetInstrument(instr)
+	mixer := synth.NewMixer()
+	mixer.AddChannel(channel)
+	synth := synth.NewSynth(cfg)
+	synth.Mixer = mixer
+	synth.EnableSDLSink()
+	ctrl := controller.NewController(cfg)
+	ctrl.Synth = synth
 
-	// Usually 44100 or 48000. Other values might cause distortions in Oto
-	samplingRate := 44100
+	// Playing individual notes with mixer.NoteOn/NoteOff produces artifacts
+	// when the notes change. So instead we play an A4 note and bend it wherever
+	// we like, and only call NoteOff when we stop playing
+	noteOn := false
 
-	// Number of channels (aka locations) to play sounds from. Either 1 or 2.
-	// 1 is mono sound, and 2 is stereo (most speakers are stereo).
-	numOfChannels := 1
-
-	// Bytes used by a channel to represent one sample. Either 1 or 2 (usually 2).
-	audioBitDepth := 2
-
-	otoCtx, readyChan, err := oto.NewContext(samplingRate, numOfChannels, audioBitDepth)
-	if err != nil {
-		panic("oto.NewContext failed: " + err.Error())
-	}
-	// It might take a bit for the hardware audio devices to be ready, so we wait on the channel.
-	<-readyChan
-	var player oto.Player = nil
-	wave := NewSineWave(0, 1.0, samplingRate)
-	log.Println(wave)
+	// And start the synthesizer in the background
+	go ctrl.StartSynth()
 	for {
 		select {
 		case note := <-notes:
-			if player != nil {
-				player.Pause()
-				if note.Freq == 0 || note.Vol == 0 {
-					continue // don't play
+			if note.Freq == 0 || note.Vol == 0 {
+				// stop playing
+				mixer.NoteOff(0, 69 /* A4 */)
+				noteOn = false
+			} else {
+				if !noteOn {
+					mixer.NoteOn(0, 69 /* A4 */, 1.0)
 				}
-				// account for unplayed samples
-				wave.input.samplesRead -= int64(player.UnplayedBufferSize() / audioBitDepth)
 			}
-			wave.input.transitionInto(SineWaveInputCoeff(note.Freq, samplingRate))
-			wave.Vol = note.Vol
-			// TODO handle note.Vol
-			player = otoCtx.NewPlayer(&wave)
-			player.Play() // async, doesn't block
+			mixer.SetPitchbend(0, float64(note.Freq) / A4)
 		case <-quit:
-			if player != nil {
-				player.Pause()
-			}
+			ctrl.Quit()
 			return
 		}
 	}
